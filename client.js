973483f191aafa2e019a4e9c4adf1e77d05d5d96
nvcompsci <31483891+nvcompsci@users.noreply.github.com>
2020-10-05 13:41:09 -0400

    basic UI, start javascript

--- client.js
+++ client.js
@@ -1,1 +1,10 @@
-console.log('script loaded')
+console.log('There is no try, only do')
+
+//add click event handler on button
+let $button = document.getElementById('addTodo')
+
+
+//define the button click handler
+function addTodo() {
+    console.log("button was clicked")
+}
