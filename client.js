80fb59a1c8a1b85a6d940d37d49e039a8ed2fa81
nvcompsci <31483891+nvcompsci@users.noreply.github.com>
2020-10-06 13:41:16 -0400

    start taskDone()

--- client.js
+++ client.js
@@ -1,22 +1,41 @@
 console.log('There is no try, only do')
 
-//add click event handler on button
+//create variables for page elements
 let $button = document.getElementById('addTodo')
 let $textBox = document.getElementById('userInput')
 let $list = document.getElementById('list')
+let $completed = document.getElementById('completed')
+//add click event handler on button
 $button.onclick = addTodo
 
 //define the button click handler
 function addTodo(event) {
-    //console.log("button was clicked")
-    //$button.style.backgroundColor = 'red'
+    console.log("button was clicked")
+    $button.style.backgroundColor = 'red'
 
     //get text from textBox
     let newTodoText = $textBox.value
     console.log(newTodoText)
     //create new list item
     let $newTodoItem = document.createElement('li')
-    $newTodoItem.innerHTML = newTodoText
+    //use String template with backticks
+    $newTodoItem.innerHTML = `<li>${newTodoText}<button onclick='taskDone(event)'>Done</button></li>`
     //put list item in list
     $list.append($newTodoItem)
+}
+
+function taskDone(event) {
+    //event.target is the element that was clicked
+    console.log(event.target)
+    $completed.append(event.target)
+}
+
+//1. variable for heading element
+let $head = document.querySelector('h1')
+//2. set 'changeColor' function as click handler on heading
+$head.onclick = changeColor
+//3.a define 'changeColor' function
+function changeColor() {
+    //3.b. set color (text) of heading to red
+    $head.style.color = 'red'
 }
