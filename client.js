e9cbcc1043de18a6bb286d9c6b4a1d9607c20455
ryantussing <56258028+ryantussing@users.noreply.github.com>
2020-10-08 09:48:57 -0400

    2nd edits
    need to adjust code for the list.append statement

--- client.js
+++ client.js
@@ -7,27 +7,30 @@ button.onclick = addTodo
 
 let textBox = document.getElementById('userInput')
 let list = document.getElementById('list')
+let completed = document.getElementById('completed')
 
 
 function addTodo() {
     console.log("button clicked")
     // button.style.backgroundColor = 'red'
-
     // get new text from textbox
     let newTodoText = textBox.value
     //create new list item
     let newTodo = document.createElement('li')
     newTodo.innerHTML = newTodoText
     //put list item in list
-    list.append(newTodo)
+    if (textBox.value = ' ' || textBox.value = '' ) {
+        list.append(newTodo)
+    }
+    textBox.value = ''
 }
 
 // variable for h1
 let h1 = document.getElementById('h1')
 // connect changeColor function as click handler
-
+h1.onclick = changeColor
 // define changecolor function
 function changeColor() {
     // set color of text to yellow
-
+    h1.style.color = 'yellow'
 }
