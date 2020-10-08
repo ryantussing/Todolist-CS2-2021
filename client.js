5a13d0efafa42098560868a0e27523b34ff82247
ryantussing <56258028+ryantussing@users.noreply.github.com>
2020-10-08 13:39:25 -0400

    3rd edits

--- client.js
+++ client.js
@@ -2,27 +2,26 @@ console.log("hello world")
 
 // create variable for button element
 let button = document.getElementById('addTodo')
-// link the button to the action of the function
-button.onclick = addTodo
-
 let textBox = document.getElementById('userInput')
 let list = document.getElementById('list')
 let completed = document.getElementById('completed')
-
+// link the button to the action of the function
+button.onclick = addTodo
 
 function addTodo() {
     console.log("button clicked")
     // button.style.backgroundColor = 'red'
     // get new text from textbox
     let newTodoText = textBox.value
-    //create new list item
-    let newTodo = document.createElement('li')
-    newTodo.innerHTML = newTodoText
-    //put list item in list
-    if (textBox.value = ' ' || textBox.value = '' ) {
+    if (newTodoText != ' ' && newTodoText != '') {
+        //create new list item
+        let newTodo = document.createElement('li')
+        newTodo.innerHTML = `${newTodoText} <button id='taskButton' onclick='markAsDone(event)'>Done</button> 
+        <button id='deleteButton' onclick='deleteTodo(event)'>Delete</button>`
+        //put list item in list
         list.append(newTodo)
+        textBox.value = ''
     }
-    textBox.value = ''
 }
 
 // variable for h1
@@ -33,4 +32,21 @@ h1.onclick = changeColor
 function changeColor() {
     // set color of text to yellow
     h1.style.color = 'yellow'
+}
+
+function markAsDone (event) {
+    console.log('todo done')
+    let todo = event.target.parentElement
+    todo.style.color = 'darkgrey'
+    todo.innerHTML = `<button id='deleteButton' onclick='deleteTodo(event)'>Delete</button>`
+    completed.append(todo)
+    let doneButton = event.target
+    doneButton.remove()
+
+}
+
+function deleteTodo(event) {
+    console.log('hello')
+    let deleteButton = event.target.parentElement
+    deleteButton.remove()
 }
